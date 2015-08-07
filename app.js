6886ef4d2191ced8038f438a9c52669e8ad972ea
Sergey Orlov <s.orlov@javad.com>
2015-08-07 16:51:48 +0300

    Used observable and templates from kendo Ui. Build a simple table.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hi")
+$(document).ready(function () {
+
+    var data = [
+        { name: "John", surname: "Doe" },
+        { name: "Jones", surname: "Smith" }
+    ]
+
+    var viewModel = kendo.observable({
+        displayList: [ ],
+        clickShowRow: function(){
+            console.log("!!!clickShowRow")
+        }
+    });
+
+    kendo.bind($("#main"), viewModel);
+    viewModel.set("displayList",data)
+});
