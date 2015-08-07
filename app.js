aa2b3ac60e419358dedd88ccb1df669697ce9638
Sergey Orlov <s.orlov@javad.com>
2015-08-07 17:35:04 +0300

    Change table template. Add more flights.

--- app.js
+++ app.js
@@ -1,17 +1,174 @@
 $(document).ready(function () {
 
     var data = [
-        { name: "John", surname: "Doe" },
-        { name: "Jones", surname: "Smith" }
+        {
+            type: "Departure",
+            flight: "AA 403",
+            airlines: "American Airlines" },
+
+        {
+            type: "Arrival",
+            flight: "UA 243",
+            airlines: "United Airlines"
+        },
+        {
+            type: "Arrival",
+            flight: "AlA 352",
+            airlines: "Alaska Airlines"
+        },
+        {
+            type: "Departure",
+            flight: "UsA 345",
+            airlines: "US Airways"
+        },
+        {
+            type: "Departure",
+            flight: "AA 403",
+            airlines: "American Airlines" },
+
+        {
+            type: "Arrival",
+            flight: "UA 243",
+            airlines: "United Airlines"
+        },
+        {
+            type: "Arrival",
+            flight: "AlA 352",
+            airlines: "Alaska Airlines"
+        },
+        {
+            type: "Departure",
+            flight: "UsA 345",
+            airlines: "US Airways"
+        },
+        {
+            type: "Departure",
+            flight: "AA 403",
+            airlines: "American Airlines" },
+
+        {
+            type: "Arrival",
+            flight: "UA 243",
+            airlines: "United Airlines"
+        },
+        {
+            type: "Arrival",
+            flight: "AlA 352",
+            airlines: "Alaska Airlines"
+        },
+        {
+            type: "Departure",
+            flight: "UsA 345",
+            airlines: "US Airways"
+        },
+        {
+            type: "Departure",
+            flight: "AA 403",
+            airlines: "American Airlines" },
+
+        {
+            type: "Arrival",
+            flight: "UA 243",
+            airlines: "United Airlines"
+        },
+        {
+            type: "Arrival",
+            flight: "AlA 352",
+            airlines: "Alaska Airlines"
+        },
+        {
+            type: "Departure",
+            flight: "UsA 345",
+            airlines: "US Airways"
+        },        {
+            type: "Departure",
+            flight: "AA 403",
+            airlines: "American Airlines" },
+
+        {
+            type: "Arrival",
+            flight: "UA 243",
+            airlines: "United Airlines"
+        },
+        {
+            type: "Arrival",
+            flight: "AlA 352",
+            airlines: "Alaska Airlines"
+        },
+        {
+            type: "Departure",
+            flight: "UsA 345",
+            airlines: "US Airways"
+        },
+        {
+            type: "Departure",
+            flight: "AA 403",
+            airlines: "American Airlines" },
+
+        {
+            type: "Arrival",
+            flight: "UA 243",
+            airlines: "United Airlines"
+        },
+        {
+            type: "Arrival",
+            flight: "AlA 352",
+            airlines: "Alaska Airlines"
+        },
+        {
+            type: "Departure",
+            flight: "UsA 345",
+            airlines: "US Airways"
+        },        {
+            type: "Departure",
+            flight: "AA 403",
+            airlines: "American Airlines" },
+
+        {
+            type: "Arrival",
+            flight: "UA 243",
+            airlines: "United Airlines"
+        },
+        {
+            type: "Arrival",
+            flight: "AlA 352",
+            airlines: "Alaska Airlines"
+        },
+        {
+            type: "Departure",
+            flight: "UsA 345",
+            airlines: "US Airways"
+        },
+        {
+            type: "Departure",
+            flight: "AA 403",
+            airlines: "American Airlines" },
+
+        {
+            type: "Arrival",
+            flight: "UA 243",
+            airlines: "United Airlines"
+        },
+        {
+            type: "Arrival",
+            flight: "AlA 352",
+            airlines: "Alaska Airlines"
+        },
+        {
+            type: "Departure",
+            flight: "UsA 345",
+            airlines: "US Airways"
+        }
+
     ]
 
     var viewModel = kendo.observable({
         displayList: [ ],
-        clickShowRow: function(){
+        clickShowRow: function () {
             console.log("!!!clickShowRow")
         }
     });
 
     kendo.bind($("#main"), viewModel);
-    viewModel.set("displayList",data)
+    viewModel.set("displayList", data)
 });
